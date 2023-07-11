8701162daa20e4406b9cc484aa805a75270fcdb5
Tyler Smith <tylersmith4596@gmail.com>
2023-07-10 19:46:37 -0700

    will prototype on vite and port to next later

--- src/app/components/welcomePopup.jsx
+++ src/app/components/welcomePopup.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import styles from './welcomePopup.module.css';
 
 export default function WelcomePopup() {
     return (
-        <dialog open>
             <div className=''>
                 <h1 className={styles.title}>Welcome to <br/>The Halls Whisper MURDER!</h1>
-                <p>This is a single player, murder mystery game where you play as "The Detective" and try to solve the murder of Dr.Warwick. When you arrive to His mansion for the dinner party, all you know is he is dead and everyone there is a suspect! <b>YOU</b> have to solve the mystery before the time runs out!</p>
-                <p>WHO?! WHERE?! HOW?!</p>
-                <p>This is where the game gets <i>spicy!</i></p>
-                <p>Each game will be different. With 10 Suspects, 7 Rooms, and 15 Weapons! there are over <b>1000</b> different stories to solve!</p>
+                <p className={styles.paraPopUp}>This is a single player, murder mystery game where you play as "The Detective" and try to solve the murder of Dr.Warwick. When you arrive to His mansion for the dinner party, all you know is he is dead and everyone there is a suspect! <b>YOU</b> have to solve the mystery before the time runs out!</p>
+                <p className={styles.paraPopUp}>WHO?! WHERE?! HOW?!</p>
+                <br/>
+                <p className={styles.paraPopUp}>This is where the game gets <i>spicy!</i></p>
+                <p className={styles.paraPopUp}>Each game will be different. With 10 Suspects, 7 Rooms, and 15 Weapons! there are over <b>1000</b> different stories to solve!</p>
             </div>
-        </dialog>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,30 @@
+'use client'
+
+import WelcomePopup from './components/welcomePopup'
 import styles from './page.module.css'
+import { useState } from 'react'
+
+const CloseWindow = () => {
+  const popup = document.querySelector('.popup')
+  popup.styles.opacity=0;
+};
 
 export default function Home() {
-  // create a function that checks if the game is rendered and if it is, it will render the game. until it is ready, it will render a loading screen
-  
-  
+
+  const [showPopup, setShowPopup] = useState(true)
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup)
+    CloseWindow()
+    console.log(showPopup)
+  }
+
   return (
     <main className={styles.main}>
+      <div className={styles.popup} showPopup>
+      <WelcomePopup className={styles.text} />
+      <div className={styles.startBtn} onClick={togglePopup}>Start</div>
+      </div>
     </main>
   )
 }
